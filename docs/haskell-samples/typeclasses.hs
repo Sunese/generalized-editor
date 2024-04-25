3afb8c91e801a39431d0a50849d7a41a7e6dd61d
Sunese <suneengtorp@live.dk>
2024-04-25 09:55:02 +0200

    docs: add more haskell samples

--- docs/haskell-samples/typeclasses.hs
+++ docs/haskell-samples/typeclasses.hs
@@ -8,6 +8,8 @@ data E = Plus E E
         | HoleE
         deriving (Show)
 
+data WellFormed = WellFormed
+
 class Substitutable a where
     substitute :: a -> a -> a
 
@@ -19,3 +21,19 @@ example = substitute 1 2
 anotherExample = substitute (Let Num ([Num], HoleS)) HoleS
 
 anotherAnotherExample = substitute Num HoleE
+
+
+class Satisfiable op where
+    at :: WellFormed -> op -> Bool
+    maybe :: WellFormed -> op -> Bool
+    necessarily :: WellFormed -> op -> Bool
+
+instance Satisfiable S where
+    at ast op = True
+    at _ _ = False
+
+    maybe _ HoleS = False
+    maybe _ _ = True
+
+    necessarily _ HoleS = False
+    necessarily _ _ = True
